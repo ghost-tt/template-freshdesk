e14fbb3c2a440bb75cab3cfd50554897de2a89ae
Ashish Sharma <Ashishbhardwaj727@gmail.com>
2021-07-26 01:56:31 +0530

    Update solutions.js

--- assets/js/solutions.js
+++ assets/js/solutions.js
@@ -1,7 +1,7 @@
 $(function () {
-    $("#headerContent").load("/views/header.html");
-    $("#navContent").load("/views/nav.html");
-    $("#bodyContent").load("/views/solutions.html");
-    $("#footerContent").load("/views/footer.html");
-    $("#copyrightContent").load("/views/copyright.html");
+    $("#headerContent").load("views/header.html");
+    $("#navContent").load("views/nav.html");
+    $("#bodyContent").load("views/solutions.html");
+    $("#footerContent").load("views/footer.html");
+    $("#copyrightContent").load("views/copyright.html");
 });
